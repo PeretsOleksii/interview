a92061e56d34323520f71634b227ba8a7c109281
PeretsOleksii <aleksey.perets@gmail.com>
2024-05-09 10:07:34 -0700

    initial commit

--- src/test/resources/insert_tutorials.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO myschema.tutorial (id, title, description, published)
-VALUES(1, 'Spring Boot Tut#1', 'Tut#1 Description', false);
-INSERT INTO myschema.tutorial (id, title, description, published)
-VALUES(2, 'Spring Boot Tut#2', 'Tut#2 Description', true);
-INSERT INTO myschema.tutorial (id, title, description, published)
-VALUES(3, 'Spring Boot Tut#3', 'Tut#3 Description', false);
