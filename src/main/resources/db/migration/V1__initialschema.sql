0b162e2521017c6686f76b96d683aedcb45bc330
PeretsOleksii <aleksey.perets@gmail.com>
2024-05-08 18:25:19 -0700

    initial commit

---
+++ src/main/resources/db/migration/V1__initialschema.sql
@@ -0,0 +1,4 @@
+CREATE TABLE IF NOT EXISTS ${schema_name}."user" (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100)
+);
---
+++ src/test/java/com/interview/InterviewApplicationTest.java
@@ -0,0 +1,14 @@
+package com.interview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@SpringBootTest
+@Testcontainers
+public class InterviewApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+}
